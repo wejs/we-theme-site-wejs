53e81361dbff7cd380a6fd50bca1d6a64bb4c94f
leonanluppi <leonan.luppi@gmail.com>
2015-08-18 09:20:16 -0300

    atualizacao dos plugins, consumir api do github

--- files/public/scripts.js
+++ files/public/scripts.js
@@ -1,1 +1,14 @@
 // Theme scripts
+$(document).ready(function() {
+
+    function byTerm(searchTerm) {
+        return searchTerm.name.indexOf('plugin') >= 0; 
+    }
+
+    $.ajax({
+        url: "https://api.github.com/orgs/wejs/repos"
+    }).then(function(data) {
+        var plugins = data.filter(byTerm);
+        console.log(plugins);
+    });
+});
