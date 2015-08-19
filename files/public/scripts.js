22dde091cd4a431c8c0ef5959e095a80393260bf
leonanluppi <leonan.luppi@gmail.com>
2015-08-19 09:31:29 -0300

    autorização via token para API do github

--- files/public/scripts.js
+++ files/public/scripts.js
@@ -1,14 +1,16 @@
 // Theme scripts
 $(document).ready(function() {
-
-    function byTerm(searchTerm) {
+    function filterPlugins(searchTerm) {
         return searchTerm.name.indexOf('plugin') >= 0; 
     }
 
     $.ajax({
-        url: "https://api.github.com/orgs/wejs/repos"
+        url: 'https://api.github.com/orgs/wejs/repos?type="public"',
+        headers: {
+            "Authorization": "token 0d6a63d6acbf17a15f2bd3b603407932b7c8425e"
+        }
     }).then(function(data) {
-        var plugins = data.filter(byTerm);
+        var plugins = data.filter(filterPlugins);
         console.log(plugins);
     });
 });
