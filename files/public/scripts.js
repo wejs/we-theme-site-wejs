1307084c801bc55faa077b1bce2254827d0a4628
leonanluppi <leonan.luppi@gmail.com>
2015-08-25 23:35:40 -0300

    remove unnecessary code at script.js

--- files/public/scripts.js
+++ files/public/scripts.js
@@ -1,16 +1,2 @@
 // Theme scripts
-$(document).ready(function() {
-    function filterPlugins(searchTerm) {
-        return searchTerm.name.indexOf('plugin') >= 0; 
-    }
-
-    $.ajax({
-        url: 'https://api.github.com/orgs/wejs/repos?type="public"',
-        headers: {
-            "Authorization": "token 9dfe4f9a7dddf513c4f546b0152a26a2738bcf85"
-        }
-    }).then(function(data) {
-        var plugins = data.filter(filterPlugins);
-        console.log(plugins);
-    });
-});
+$(document).ready(function() {});
